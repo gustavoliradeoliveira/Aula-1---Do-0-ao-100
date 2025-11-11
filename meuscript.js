fa537691cfc10e7c4f2535bd2de7585f845bd68c
gustavoliradeoliveira <135131490+gustavoliradeoliveira@users.noreply.github.com>
2025-11-10 21:48:32 -0300

    Add files via upload

---
+++ meuscript.js
@@ -0,0 +1,14 @@
+function getDados(){
+    let dados = document.querySelector('#email');
+    document.getElementById('mail').innerHTML =dados.value;
+}
+function getDados2(){
+    let dados = document.querySelectorAll('input'); // busca todas as inputs
+    let divDados = document.getElementById('dados');
+    for(let i=0; i<dados.length; i++){
+        let paragrafo = document.createElement('p');
+        paragrafo.innerHTML = dados[i].value;
+        divDados.appendChild(paragrafo);
+
+    }
+}
